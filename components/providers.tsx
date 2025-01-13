dafc555feed4d4a41ddee98c8cae4d7d6a0b2f19
Matthias <me@motz.xyz>
2025-01-13 10:25:53 +0000

    set up posthog rewrites

--- components/providers.tsx
+++ components/providers.tsx
@@ -10,7 +10,8 @@ import { useState } from "react"
 function CSPostHogProvider({ children }: { children: React.ReactNode }) {
     if (typeof window !== "undefined") {
         posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-            api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+            api_host: "/ingest",
+            ui_host: process.env.NEXT_PUBLIC_POSTHOG_UI_HOST,
             person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
         })
     }
--- next.config.ts
+++ next.config.ts
@@ -11,4 +11,22 @@ export default {
             },
         ],
     },
+    async rewrites() {
+        return [
+            {
+                source: "/ingest/static/:path*",
+                destination: "https://eu-assets.i.posthog.com/static/:path*",
+            },
+            {
+                source: "/ingest/:path*",
+                destination: "https://eu.i.posthog.com/:path*",
+            },
+            {
+                source: "/ingest/decide",
+                destination: "https://eu.i.posthog.com/decide",
+            },
+        ]
+    },
+    // This is required to support PostHog trailing slash API requests
+    skipTrailingSlashRedirect: true,
 } satisfies NextConfig
