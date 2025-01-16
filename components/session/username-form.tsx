6cb0e8e1815646990e8a3dd86056b555716d82c4
motz0815 <me@motz.xyz>
2025-01-16 21:36:04 +0100

    improve visibility of username form

--- components/session/username-form.tsx
+++ components/session/username-form.tsx
@@ -6,7 +6,12 @@ export function UsernameForm() {
     return (
         <form action={setUsername}>
             <div className="flex flex-col gap-2">
-                <Input type="text" name="username" />
+                <Input
+                    type="text"
+                    name="username"
+                    placeholder="Enter username..."
+                    className="bg-background text-foreground"
+                />
                 <SubmitButton>Set Username</SubmitButton>
             </div>
         </form>
