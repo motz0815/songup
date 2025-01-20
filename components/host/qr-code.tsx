257be4c87c67b3850cf2df1f85f53cc974c693d0
motz0815 <me@motz.xyz>
2025-01-20 16:11:32 +0100

    add utm source parameter to qr code

--- components/host/qr-code.tsx
+++ components/host/qr-code.tsx
@@ -4,7 +4,7 @@ import { getURL } from "@/lib/utils"
 import { QRCodeSVG } from "qrcode.react"
 
 export function RoomQRCode({ roomCode }: { roomCode: string }) {
-    const url = getURL(`/room/${roomCode}`)
+    const url = getURL(`/room/${roomCode}?utm_source=qr-code`)
 
     return (
         <div className="flex items-center justify-center">
