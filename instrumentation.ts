b927a6a198eb3c1472a78140db70926d522ecc82
motz0815 <me@motz.xyz>
2025-02-07 12:25:15 +0100

    Add PostHog server-side exception autocapture

---
+++ instrumentation.ts
@@ -0,0 +1,6 @@
+import getPostHog from "./lib/posthog"
+
+export function register() {
+    // Register the PostHog client
+    getPostHog()
+}
--- lib/posthog.ts
+++ lib/posthog.ts
@@ -2,9 +2,10 @@ import { PostHog } from "posthog-node"
 
 export default function getPostHog() {
     const posthogClient = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-        host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+        host: process.env.NEXT_PUBLIC_POSTHOG_UI_HOST,
         flushAt: 1,
         flushInterval: 0,
+        enableExceptionAutocapture: true,
     })
     return posthogClient
 }
