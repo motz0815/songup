ed6eeab41fdc33f1625423482c3cd3595a8a98a6
motz0815 <me@motz.xyz>
2025-07-30 16:34:49 +0200

    what about this?

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
                 destination:
                     process.env.NODE_ENV === "development"
                         ? "http://127.0.0.1:5328/api/flask/:path*"
-                        : "/api/index",
+                        : "/flask/index",
             },
         ]
     },
