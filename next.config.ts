78a61bbbc37f5dd9e0a96236876c2dd2eb7941aa
motz0815 <me@motz.xyz>
2025-07-30 15:42:11 +0200

    try this

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,10 @@ const nextConfig: NextConfig = {
                         ? "http://127.0.0.1:5328/api/flask/:path*"
                         : "/api/index",
             },
+            {
+                source: "/api/:path*",
+                destination: "/njsapi/:path*",
+            },
         ]
     },
 }
