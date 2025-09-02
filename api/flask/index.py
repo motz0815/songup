d2baf066bf35ef6269f1e92ddb16ff84e57cf49e
motz0815 <me@motz.xyz>
2025-09-02 21:07:30 +0200

    Revert "debug print flask search"
    This reverts commit 70664e033952367ed3157927fbaa229f559afd8c.

--- api/flask/index.py
+++ api/flask/index.py
@@ -32,7 +32,6 @@ def search():
     # In my rudimentary test a request took 1.17s without the filter, while it took 2.93s with filtering enabled.
     # But on the other hand, we kind of *need* to check this, because otherwise we'll get songs that are not embeddable and will fail in the last second.
     results = [result for result in results if yt.get_song(result["videoId"])["playabilityStatus"].get("playableInEmbed", False)]
-    print(results)
     return jsonify(results)
 
 @app.route("/flask/search-playlist", methods=["GET"])
