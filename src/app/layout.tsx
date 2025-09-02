ab7dfde599bd7b76334041d4b09d4b44dbdb020a
Matthias <me@motz.xyz>
2025-09-02 08:52:18 +0200

    better toast colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import { Providers } from "@/components/providers"
 import { Toaster } from "@/components/ui/sonner"
 import type { Metadata, Viewport } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
-import { Suspense } from "react"
 import "./globals.css"
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] })
@@ -35,9 +34,7 @@ export default function RootLayout({
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
                 <Providers>{children}</Providers>
-                <Suspense>
-                    <Toaster />
-                </Suspense>
+                <Toaster richColors theme="light" />
             </body>
         </html>
     )
