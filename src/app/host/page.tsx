fd285bb812aab1aaff9b44bcad0c7fbbeee04c94
motz0815 <me@motz.xyz>
2025-09-04 08:28:59 +0200

    Fix own rooms query

--- src/app/host/page.tsx
+++ src/app/host/page.tsx
@@ -1,9 +1,16 @@
 import { api } from "@/convex/_generated/api"
+import { convexAuthNextjsToken } from "@convex-dev/auth/nextjs/server"
 import { preloadQuery } from "convex/nextjs"
 import ManageRooms from "./manage"
 
 export default async function ManageRoomsPage() {
-    const preloadedRooms = await preloadQuery(api.rooms.manage.listOwnRooms)
+    const preloadedRooms = await preloadQuery(
+        api.rooms.manage.listOwnRooms,
+        {},
+        {
+            token: await convexAuthNextjsToken(),
+        },
+    )
 
     return <ManageRooms preloadedRooms={preloadedRooms} />
 }
