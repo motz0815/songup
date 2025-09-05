04ddcfc284d8242962b4aa8ec3d4a18d66b38b0e
motz0815 <me@motz.xyz>
2025-09-05 12:17:19 +0200

    Improve room page layout for mobile

--- src/components/room/queue.tsx
+++ src/components/room/queue.tsx
@@ -4,7 +4,7 @@ import { api } from "@/convex/_generated/api"
 import { Id } from "@/convex/_generated/dataModel"
 import { useAutoAnimate } from "@formkit/auto-animate/react"
 import { useQuery } from "convex/react"
-import { SongCard } from "../songs/song-card"
+import { ImageWithFallback } from "../image-with-fallback"
 import { ScrollArea } from "../ui/scroll-area"
 
 export function Queue({ roomId }: { roomId: Id<"rooms"> }) {
@@ -15,12 +15,29 @@ export function Queue({ roomId }: { roomId: Id<"rooms"> }) {
     const [animationParent] = useAutoAnimate()
 
     return (
-        <ScrollArea className="h-full grow rounded-lg border border-white/20 bg-white/10 p-4 shadow-md backdrop-blur-lg">
+        <ScrollArea className="h-full max-h-[40vh] grow overflow-y-auto rounded-lg border border-white/20 bg-white/10 p-4 shadow-md backdrop-blur-lg">
             <ul ref={animationParent} className="space-y-4">
                 {queue && queue.length > 0 ? (
                     queue.map((song) => (
                         <li key={song._id}>
-                            <SongCard song={song} />
+                            <div className="group relative flex items-center justify-between space-x-4 rounded-lg border border-white/20 bg-white/10 p-3 shadow-md transition-all">
+                                <ImageWithFallback
+                                    src={`https://i.ytimg.com/vi_webp/${song.videoId}/mqdefault.webp`}
+                                    width={128}
+                                    height={128}
+                                    alt={`${song.title}`}
+                                    className="aspect-video h-20 rounded-lg border border-white/20 object-cover"
+                                    unoptimized
+                                />
+                                <div className="w-full">
+                                    <h4 className="text-lg font-semibold text-shadow-md md:text-xl">
+                                        {song.title}
+                                    </h4>
+                                    <p className="text-sm text-gray-100 text-shadow-sm md:text-lg">
+                                        {song.artist}
+                                    </p>
+                                </div>
+                            </div>
                         </li>
                     ))
                 ) : (
