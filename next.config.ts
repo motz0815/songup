1b32967ca3d1a2a0f40e37fbef410707ab6693b3
motz0815 <me@motz.xyz>
2025-09-08 13:55:09 +0200

    Add PostHog

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,14 @@ const nextConfig: NextConfig = {
                         ? "http://127.0.0.1:5328/flask/:path*"
                         : "/api/flask/index",
             },
+            {
+                source: "/relay-iljT/static/:path*",
+                destination: "https://eu-assets.i.posthog.com/static/:path*",
+            },
+            {
+                source: "/relay-iljT/:path*",
+                destination: "https://eu.i.posthog.com/:path*",
+            },
         ]
     },
     images: {
@@ -19,6 +27,7 @@ const nextConfig: NextConfig = {
             },
         ],
     },
+    skipTrailingSlashRedirect: true,
 }
 
 export default nextConfig
