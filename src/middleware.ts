575531fdef9ecb7b4f7b954ee1d99d5b120ac814
Matthias <me@motz.xyz>
2025-09-09 15:24:30 +0200

    Set auth cookie max age to 30 days
    This is a bug fix, because right now cookies expire on session end because of an oversight and that logs out users after a browser restart

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,10 @@
 import { convexAuthNextjsMiddleware } from "@convex-dev/auth/nextjs/server"
 
-export default convexAuthNextjsMiddleware()
+export default convexAuthNextjsMiddleware((request, { convexAuth }) => {}, {
+    cookieConfig: {
+        maxAge: 60 * 60 * 24 * 30, // 30 days cookie expiration
+    },
+})
 
 export const config = {
     // The following matcher runs middleware on all routes
