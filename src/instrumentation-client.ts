8e2da104341f1d0edc04f5c2a689ec517e0c9336
motz0815 <me@motz.xyz>
2026-03-23 12:30:17 +0100

    updated posthog defaults

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -4,5 +4,5 @@ posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     api_host: "/relay-iljT",
     ui_host: "https://eu.posthog.com",
     cookieless_mode: "always",
-    defaults: "2025-05-24",
+    defaults: "2026-01-30",
 })
