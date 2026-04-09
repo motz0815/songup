428b8d167f1e6b686962073f843ef1cb2193df91
motz0815 <me@motz.xyz>
2026-04-09 15:00:24 +0900

    Optimize room stats

--- src/convex/stats.ts
+++ src/convex/stats.ts
@@ -1,6 +1,6 @@
 import { OssStats } from "@erquhart/convex-oss-stats"
-import { components, internal } from "./_generated/api"
-import { internalAction, internalMutation, query } from "./_generated/server"
+import { components } from "./_generated/api"
+import { MutationCtx, query } from "./_generated/server"
 
 export const ossStats = new OssStats(components.ossStats, {
     githubRepos: ["motz0815/songup"],
@@ -10,47 +10,6 @@ export const ossStats = new OssStats(components.ossStats, {
 })
 
 export const { sync, getGithubRepo, getGithubOwner } = ossStats.api()
-
-export const updateStats = internalAction({
-    handler: async (ctx) => {
-        const headers = {
-            Authorization: `Bearer ${process.env.POSTHOG_PERSONAL_API_KEY!}`,
-            "Content-Type": "application/json",
-        }
-
-        const stats = await fetch(
-            "https://eu.posthog.com/api/environments/46839/endpoints/total-rooms/run",
-            { headers },
-        )
-        const data = await stats.json()
-        if (data.results[0]) {
-            await ctx.runMutation(internal.stats.insert, {
-                roomsCreated: data.results[0][0],
-                songsAdded: data.results[0][1],
-            })
-        }
-    },
-})
-
-export const insert = internalMutation({
-    handler: async (
-        ctx,
-        data: { roomsCreated: number; songsAdded: number },
-    ) => {
-        const stats = await ctx.db.query("stats").first()
-        if (!stats) {
-            await ctx.db.insert("stats", {
-                roomsCreated: 0,
-                songsAdded: 0,
-            })
-            return
-        }
-        await ctx.db.patch("stats", stats._id, {
-            roomsCreated: data.roomsCreated,
-            songsAdded: data.songsAdded,
-        })
-    },
-})
 
 export const getRoomStats = query({
     handler: async (ctx) => {
@@ -64,3 +23,33 @@ export const getRoomStats = query({
         return stats
     },
 })
+
+export async function countRoomCreated(ctx: MutationCtx) {
+    const stats = await ctx.db.query("stats").first()
+    if (!stats) {
+        await ctx.db.insert("stats", {
+            roomsCreated: 1,
+            songsAdded: 0,
+        })
+        return
+    }
+    await ctx.db.patch("stats", stats._id, {
+        roomsCreated: stats.roomsCreated + 1,
+    })
+    return stats.roomsCreated + 1
+}
+
+export async function countSongAdded(ctx: MutationCtx) {
+    const stats = await ctx.db.query("stats").first()
+    if (!stats) {
+        await ctx.db.insert("stats", {
+            roomsCreated: 0,
+            songsAdded: 1,
+        })
+        return
+    }
+    await ctx.db.patch("stats", stats._id, {
+        songsAdded: stats.songsAdded + 1,
+    })
+    return stats.songsAdded + 1
+}
