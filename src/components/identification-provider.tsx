b70d5269a76c1ccb5b48a9e1baf76f6273417b8c
motz0815 <me@motz.xyz>
2026-04-22 10:17:43 +0200

    user identification

---
+++ src/components/identification-provider.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import { api } from "@/convex/_generated/api"
+import { useQuery } from "convex/react"
+import posthog from "posthog-js"
+import { useEffect } from "react"
+
+export function IdentificationProvider({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    const user = useQuery(api.auth.getCurrentUser)
+
+    useEffect(() => {
+        if (!user?.isAnonymous && user?._id) posthog.identify(user._id)
+    }, [user?._id])
+
+    return <>{children}</>
+}
--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,10 +1,13 @@
 import { ConvexAuthNextjsServerProvider } from "@convex-dev/auth/nextjs/server"
 import { ConvexClientProvider } from "./convex-client-provider"
+import { IdentificationProvider } from "./identification-provider"
 
 export function Providers({ children }: { children: React.ReactNode }) {
     return (
         <ConvexAuthNextjsServerProvider>
-            <ConvexClientProvider>{children}</ConvexClientProvider>
+            <ConvexClientProvider>
+                <IdentificationProvider>{children}</IdentificationProvider>
+            </ConvexClientProvider>
         </ConvexAuthNextjsServerProvider>
     )
 }
