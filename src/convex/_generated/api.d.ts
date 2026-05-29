a46b03561aed2ab5b3bb9cb9597a46f4af904047
motz0815 <me@motz.xyz>
2026-05-29 12:41:36 +0200

    Properly register signup event

--- src/convex/_generated/api.d.ts
+++ src/convex/_generated/api.d.ts
@@ -8,6 +8,7 @@
  * @module
  */
 
+import type * as analytics_users from "../analytics/users.js";
 import type * as auth from "../auth.js";
 import type * as crons from "../crons.js";
 import type * as functions from "../functions.js";
@@ -26,6 +27,7 @@ import type {
 } from "convex/server";
 
 declare const fullApi: ApiFromModules<{
+  "analytics/users": typeof analytics_users;
   auth: typeof auth;
   crons: typeof crons;
   functions: typeof functions;
--- src/convex/auth.ts
+++ src/convex/auth.ts
@@ -1,7 +1,8 @@
 import Google from "@auth/core/providers/google"
 import { Anonymous } from "@convex-dev/auth/providers/Anonymous"
 import { convexAuth, getAuthUserId } from "@convex-dev/auth/server"
-import { query } from "./_generated/server"
+import { internal } from "./_generated/api"
+import { MutationCtx, query } from "./_generated/server"
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
     providers: [Anonymous, Google],
@@ -9,6 +10,30 @@ export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
         customClaims: async (ctx, { userId }) => {
             const user = await ctx.db.get("users", userId)
             return { userId, email: user?.email, name: user?.name }
+        },
+    },
+    callbacks: {
+        // Trigger the user signed up event after the user is created
+        async afterUserCreatedOrUpdated(
+            ctx: MutationCtx,
+            { userId, profile, provider },
+        ) {
+            if (
+                !provider ||
+                provider.type !== "oauth" ||
+                provider.name !== "Google"
+            )
+                return
+
+            await ctx.scheduler.runAfter(
+                0,
+                internal.analytics.users.userSignedUp,
+                {
+                    userId,
+                    email: profile?.email,
+                    name: profile?.name as string | undefined,
+                },
+            )
         },
     },
 })
