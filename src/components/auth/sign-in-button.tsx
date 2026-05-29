cedbd9a28409bb724eceddc3295a14849b3f33ff
motz0815 <me@motz.xyz>
2026-05-29 12:19:37 +0200

    PostHog improvements

--- src/components/auth/sign-in-button.tsx
+++ src/components/auth/sign-in-button.tsx
@@ -1,4 +1,7 @@
+"use client"
+
 import { useAuthActions } from "@convex-dev/auth/react"
+import posthog from "posthog-js"
 import { FaGoogle } from "react-icons/fa"
 import { Button, ButtonProps } from "../ui/button"
 
@@ -7,7 +10,10 @@ export function SignInButton(props?: Omit<ButtonProps, "onClick">) {
     return (
         <Button
             {...props}
-            onClick={() => void signIn("google", { redirectTo: "/host" })}
+            onClick={() => {
+                posthog.capture("signin_clicked")
+                void signIn("google", { redirectTo: "/host" })
+            }}
         >
             <FaGoogle className="size-4" />
             Sign in with Google
